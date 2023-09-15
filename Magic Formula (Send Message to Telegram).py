3ad75f58de40dfa6f465858db47b5d7549fa0a64
operdigao <robson.perdigao@outlook.com>
2023-09-14 21:56:27 -0300

    organization and include sectors

--- Magic Formula (Send Message to Telegram).py
+++ Magic Formula (Send Message to Telegram).py
@@ -2,6 +2,22 @@ import pandas as pd
 import requests
 import telebot
 
+def lista_setores(setor=None):
+    """
+    Setor: ...
+
+    Output:
+      List
+    """
+    url = f'http://www.fundamentus.com.br/resultado.php?setor={setor}'
+    header = {'User-agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201' ,
+           'Accept': 'text/html, text/plain, text/css, text/sgml, */*;q=0.01' ,
+           'Accept-Encoding': 'gzip, deflate' ,
+           }
+    r = requests.get(url, headers=header)
+    df = pd.read_html(r.text,  decimal=',', thousands='.')[0]
+    return list(df['Papel'])
+  
 #IMPORTANDO DADOS DAS EMPRESAS
 url = 'http://www.fundamentus.com.br/resultado.php'
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"}
@@ -15,19 +31,22 @@ for coluna in ['Div.Yield', 'Mrg Ebit', 'Mrg. Líq.', 'ROIC', 'ROE', 'Cresc. Rec
   tabela[coluna] = tabela[coluna].str.rstrip('%').astype('float') / 100
 
 #CRIANDO RANKING
+liquidez = 1000000
+qtd_ativos = 15
 tabela = tabela[['Papel', 'Cotação', 'EV/EBIT', 'ROIC', 'Liq.2meses', 'P/L']]
 tabela['Empresa'] = tabela['Papel'].str[:4]
 tabela = tabela.drop_duplicates(subset='Empresa')
 tabela = tabela.set_index('Papel')
-tabela = tabela[tabela['Liq.2meses'] > 1000000]
+tabela = tabela[tabela['Liq.2meses'] > liquidez]
 tabela = tabela[tabela['P/L'] > 0]
 tabela = tabela[tabela['EV/EBIT'] > 0]
 tabela = tabela[tabela['ROIC'] > 0]
+tabela = tabela.drop(columns = ['Empresa', 'P/L', 'Liq.2meses'])
 tabela['RANKING_EV/EBIT'] = tabela['EV/EBIT'].rank(ascending = True)
 tabela['RANKING_ROIC'] = tabela['ROIC'].rank(ascending = False)
 tabela['RANKING_TOTAL'] = tabela['RANKING_EV/EBIT'] + tabela['RANKING_ROIC']
 tabela = tabela.sort_values('RANKING_TOTAL')
-tabela = tabela.head(15)
+tabela = tabela.head(qtd_ativos)
 
 ranking = tabela.index
 ranking = '\n'.join(f'{i+1}. {acao}' for i, acao in enumerate(ranking))
